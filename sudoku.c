ea29c50e30438595a46d729e34c03ca6d0e5490a
Replit user <>
2023-05-05 23:15:10 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -50,8 +50,45 @@ int is_valid(Node* n){
 
 
 List* get_adj_nodes(Node* n){
-    List* list=createList();
-    return list;
+  List* list=createList();
+  int i, j;
+  for(i  = 0; i < 3 ; i++){
+    for(j = 0 ; j < 3 ; j++){
+      if(n->sudo[i][j] == 0){
+        break;
+      }
+    }
+    if(n->sudo[i][j] == 0){
+      break;
+    }
+  }
+  if(i > 0){
+    Node* adj_node = copy(n);
+    adj_node->sudo[i][j] = adj_node->sudo[i-1][j];
+    adj_node->sudo[i-1][j] = 0;
+    pushBack(list, adj_node);
+  }
+  if(i < 2){
+    Node* adj_node = copy(n);
+    adj_node->sudo[i][j] = adj_node->sudo[i+1][j];
+    adj_node->sudo[i+1][j] = 0;
+    pushBack(list, adj_node);
+  }
+
+  if(j > 0){
+    Node* adj_node = copy(n);
+    adj_node->sudo[i][j] = adj_node->sudo[i][j-1];
+    adj_node->sudo[i][j-1] = 0;
+    pushBack(list, adj_node);
+  }
+  if(j < 2){
+    Node* adj_node = copy(n);
+    adj_node->sudo[i][j] = adj_node->sudo[i][j+1];
+    adj_node->sudo[i][j+1] = 0;
+    pushBack(list, adj_node);
+  }
+  
+  return list;
 }
 
 
